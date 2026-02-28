a01582af46ef95377ee66cd9322345887ba6a23c
Redkuroo <02jsmella@gmail.com>
2026-02-28 14:40:39 +0800

    clean start

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Netflix</Text>
+
     </View>
   );
 }
