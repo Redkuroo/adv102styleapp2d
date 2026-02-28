375ef71b83e79005452eb46072d12b3031999818
ianianian21 <ianbenedict.peteros@hcdc.edu.ph>
2026-02-28 15:27:40 +0800

    top view sa home

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,56 @@
-import { Image, Text, View } from "react-native";
+import { Image, Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-    >
+    <View style={styles.container}>
+<View style={{flexDirection: 'row'}}>
+
       <Text>Netflix</Text>
     <Image 
       source={require('../assets/images/netflix.png')} 
+      style={styles.image}
       />
-      <Image 
-      source={require('../assets/images/dl.png')} 
-      /><Image 
-      source={require('../assets/images/bell.png')} 
-      />
-     <Text style={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>Home</Text>
-
+      <Text style={{color: 'white', fontSize: 20, fontWeight: 'bold', padding: 30}}>Home</Text>
+<View style={styles.iconContainer}>
+          <Image 
+            source={require('../assets/images/dl.png')} 
+            style={styles.icon}
+          />
+          <Image 
+            source={require('../assets/images/bell.png')} 
+            style={styles.icon}
+          />
+        </View>
+     
+</View>
       </View>
     );
   }
+
+  const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000', 
+  },
+  logo: {
+    width: 30, // Smaller width for just the "N" symbol
+    height: 40,
+    marginRight: 10, // Space between logo and "Home"
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    width: 22,
+    height: 22,
+    marginLeft: 20,
+    tintColor: 'white',
+  },
+  image: {
+    width: 65,
+    height: 65,
+    marginTop: 20,
+    marginStart: 20,
+  },
+});
